174fd5a27e58fa71fe3758b66207fb879277d1bb
Gavin Kerr <kerrgv@gmail.com>
2025-04-10 09:51:28 -0600

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
-import tailwindcss from '@tailwindcss/vite';
  
 export default defineConfig({
   plugins: [
     react(),
-    TanStackRouterVite(),
-    tailwindcss(),
+    TanStackRouterVite()
   ],
   resolve: {
     alias: {
